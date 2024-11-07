9e73acc756281824f4713a7878d3c4c71cb08f13
Hansangjin98 <onetable98@naver.com>
2024-11-08 01:22:52 +0900

    [Feat] #9 - SceneDelegate에서 의존성 객체 등록 및 초기 화면 연결 로직 구현

--- Heim/Application/Application/Source/SceneDelegate.swift
+++ Heim/Application/Application/Source/SceneDelegate.swift
@@ -5,20 +5,74 @@
 //  Created by 정지용 on 11/5/24.
 //
 
+import Core
+import DataModule
+import DataStorage
+import Domain
+import Presentation
+import NetworkModule
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
-  func scene(
-    _ scene: UIScene,
-    willConnectTo session: UISceneSession,
-    options connectionOptions: UIScene.ConnectionOptions
-  ) {
-    guard let _ = (
-      scene as? UIWindowScene
-    ) else {
-      return
+  // MARK: - Properties
+  var window: UIWindow?
+  private var navigationController: UINavigationController?
+  // TODO: AppCoordinator 구현 후 적용 예정
+  //  private var appCoordinator: AppCoordinator?
+  
+  // MARK: - Methods
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    
+    self.navigationController = UINavigationController()
+    window?.rootViewController = navigationController
+    window?.makeKeyAndVisible()
+    
+    dependencyAssemble()
+    startScene()
+  }
+}
+
+  // MARK: - Private Extenion
+private extension SceneDelegate {
+  func dependencyAssemble() {
+    dataAssemble()
+    domainAssemble()
+    presentationAssemble()
+  }
+  
+  func dataAssemble() {
+    DIContainer.shared.register(type: SettingRepository.self) { _ in
+      return DefaultSettingRepository()
     }
+  }
+  
+  func domainAssemble() {
+    DIContainer.shared.register(type: SettingUseCase.self) { container in
+      guard let settingRepository = container.resolve(type: SettingRepository.self) else {
+        return
+      }
+      
+      return DefaultSettingUseCase(settingRepository: settingRepository)
+    }
+  }
+  
+  func presentationAssemble() {
+    guard let navigationController else { return }
+    
+    DIContainer.shared.register(type: SettingCoordinator.self) { _ in
+      return DefaultSettingCoordinator(navigationController: navigationController)
+    }
+  }
+  
+  func startScene() {
+    // TODO: AppCoordinator 구현 후 적용 예정
+//    guard let appCoordinator = DIContainer.shared.resolve(type: AppCoordinator.self) else { return }
+//    self.appCoordinator = appCoordinator
+//    appCoordinator.start()
+    guard let settingCoordinator = DIContainer.shared.resolve(type: SettingCoordinator.self) else { return }
+    settingCoordinator.start()
   }
 }
