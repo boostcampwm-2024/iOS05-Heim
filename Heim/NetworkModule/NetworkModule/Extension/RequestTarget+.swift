74ea6f34e07a2753c8ca31760369a244b4591ad4
Jiyong <hickerd@sch.ac.kr>
2024-11-12 18:28:28 +0900

    [Feat] #17 - Add extension about NetworkModule

---
+++ Heim/NetworkModule/NetworkModule/Extension/RequestTarget+.swift
@@ -0,0 +1,29 @@
+//
+//  RequestTarget+.swift
+//  NetworkModule
+//
+//  Created by 정지용 on 11/12/24.
+//
+
+import DataModule
+import Foundation
+
+extension RequestTarget {
+  var query: [String: Any] {
+    return [:]
+  }
+  
+  func makeURLRequest() throws -> URLRequest {
+    var request: URLRequest = try URLRequest(path, query: query)
+    
+    request.makeURLHeaders(headers)
+    request.httpMethod = method.rawValue
+    request.cachePolicy = .reloadIgnoringLocalCacheData
+    
+    if let body {
+      request.setBody(body)
+    }
+    
+    return request
+  }
+}
