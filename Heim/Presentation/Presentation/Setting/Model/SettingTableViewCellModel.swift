17c4315b1d3481200416bf80b68fa755e887e27a
Hansangjin98 <onetable98@naver.com>
2024-11-13 12:38:32 +0900

    [Feat] #9 - 설정 옵션 관련 Model 구현

---
+++ Heim/Presentation/Presentation/Setting/Model/SettingTableViewCellModel.swift
@@ -0,0 +1,32 @@
+//
+//  SettingItem.swift
+//  Presentation
+//
+//  Created by 한상진 on 11/13/24.
+//
+
+enum SettingIcon: String {
+  case smileIcon 
+  case cloudIcon
+  case trashIcon
+  case infoIcon
+  case helpIcon
+}
+
+struct SettingItem {
+  // MARK: - Properties
+  let title: String
+  let icon: SettingIcon
+  
+  // MARK: - Methods
+  static func defaultItems() -> [SettingItem] {
+    return [
+      SettingItem(title: "이름", icon: .smileIcon),
+      SettingItem(title: "iCloud 동기화", icon: .cloudIcon),
+      SettingItem(title: "캐시 삭제", icon: .trashIcon),
+      SettingItem(title: "데이터 초기화", icon: .trashIcon),
+      SettingItem(title: "앱 버전", icon: .infoIcon),
+      SettingItem(title: "문의하기", icon: .helpIcon),
+    ]
+  }
+}
