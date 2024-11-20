0706344919891fd11cfcf631c911427e7379180f
ParkSeongGeun <phd0801@naver.com>
2024-11-21 02:54:46 +0900

    [Feat] #36 Create DiaryDetailViewModel

--- Heim/Presentation/Presentation/Detail/DiaryDetail/ViewModel/DiaryDetailViewModel.swift
+++ Heim/Presentation/Presentation/Detail/DiaryDetail/ViewModel/DiaryDetailViewModel.swift
@@ -12,29 +12,51 @@ import Domain
 final class DiaryDetailViewModel: ViewModel {
   // MARK: - Properties
   enum Action {
-    
+    case musicRecommendation
+    case heimReply
+    case replayVoice
+    case close
   }
   
   struct State: Equatable {
-    
+    var date: String = ""
+    var description: String = ""
+    var content: String = ""
   }
   
-  // let useCase: DataStorageUseCase
   @Published var state: State
+  // private let useCase: DataStorageUseCase
+  private let timeStamp: String
   
   // MARK: - Initializer
   init(
     // useCase: DataStorageUseCase
+    timeStamp: String
   ) {
     // self.useCase = useCase
     state = State()
+    self.timeStamp = timeStamp
   }
   
   // MARK: - Methods
   func action(_ action: Action) {
     switch action {
-      
+    case .musicRecommendation, .heimReply, .replayVoice, .close:
+      break
     }
+  }
+  
+  // TODO: 기능 구현
+  func setUp() async {
+    state.date = timeStamp
+//    do {
+//      let diary = try await useCase.readDiary(timeStamp: timeStamp)
+//      state.date = timeStamp // 20241121만 추출하도록
+//      state.description = diary.emotion
+//      state.content = diary.Summary.text
+//    } catch {
+//      // TODO: Error Handling
+//    }
   }
 }
 
