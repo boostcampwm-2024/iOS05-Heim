a1d7218b348038434866339de2a6e16aacb388d5
Jiyong <hickerd@sch.ac.kr>
2024-11-20 19:42:24 +0900

    [Fix] #43 - Fix access modifier

--- Heim/Domain/Domain/Error/JSONError.swift
+++ Heim/Domain/Domain/Error/JSONError.swift
@@ -5,11 +5,11 @@
 //  Created by 정지용 on 11/19/24.
 //
 
-enum JSONError: Error {
+public enum JSONError: Error {
   case decodingError
   case encodingError
   
-  var description: String {
+  public var description: String {
     switch self {
     case .decodingError: return "파싱 중 오류 발생"
     case .encodingError: return "파싱 중 오류 발생"
