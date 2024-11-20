ed4fc11c22f2e557a2853ca04612b8d92bcce969
Hansangjin98 <onetable98@naver.com>
2024-11-20 20:35:03 +0900

    [Style] #42 - 불필요 주석 제거

--- Heim/Domain/Domain/UseCase/EmotionClassifyUseCase.swift
+++ Heim/Domain/Domain/UseCase/EmotionClassifyUseCase.swift
@@ -13,7 +13,6 @@ protocol EmotionClassifyUseCase {
 
 struct DefaultEmotionClassifyUseCase: EmotionClassifyUseCase {
   func validate(_ input: String) async throws -> Emotion {
-    // TODO: EmotionClassifier는 변경 예정 (현재 Mock Model)
     let model = try EmotionClassifier(configuration: MLModelConfiguration())
     let result = try model.prediction(text: input)
     return Emotion(rawValue: result.label) ?? .none
