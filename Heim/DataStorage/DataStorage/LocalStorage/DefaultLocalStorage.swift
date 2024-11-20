336dd033bb02b59745582f125090e5dd57b937eb
ParkSeongGeun <phd0801@naver.com>
2024-11-21 00:41:39 +0900

    [Fix] #38 Change Error Naming, Error Handling
    - IOError -> StorageError로 변경
        - 사용자에게 제공될 에러인데, Storage 기능을 사용하면서 나타내는 에러는 StorageError의 네이밍이 더욱 적절하다고 판단하였습니다.
    - 기본 throws 구현된 메서드에 한해 StorageError를 던지도록 수정

--- Heim/DataStorage/DataStorage/LocalStorage/DefaultLocalStorage.swift
+++ Heim/DataStorage/DataStorage/LocalStorage/DefaultLocalStorage.swift
@@ -15,47 +15,47 @@ public final class DefaultLocalStorage: DataStorageInterface {
   private var baseURL: URL
   private let encoder = JSONEncoder()
   private let decoder = JSONDecoder()
-
+  
   public init() {
     self.fileManager = FileManager.default
     self.baseURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
   }
-
+  
   public func readDiary<T: Decodable>(timeStamp: String) throws -> T {
     let (directory, fileName) = try parseTimeStamp(timeStamp)
     let url: URL
-
+    
     if #available(iOS 16.0, *) {
       url = baseURL.appending(path: directory, directoryHint: .isDirectory)
     } else {
       url = baseURL.appendingPathComponent(directory, isDirectory: true)
     }
-
+    
     let fileURL = url.appendingPathComponent(fileName)
-
+    
     guard fileManager.fileExists(atPath: fileURL.path) else {
-      throw IOError.readError
+      throw StorageError.readError
     }
-
+    
     guard let jsonData = fileManager.contents(atPath: fileURL.path) else {
-      throw IOError.readError
+      throw StorageError.readError
     }
-
+    
     do {
       let data = try decoder.decode(T.self, from: jsonData)
       return data
     } catch {
-      throw IOError.readError
+      throw StorageError.readError
     }
   }
-
+  
   public func saveDiary<T>(
     timeStamp: String,
     data: T
   ) throws where T: Codable {
     let (directory, fileName) = try parseTimeStamp(timeStamp)
     let url: URL
-
+    
     // 디렉토리 생성 관련, appending 메서드를 버전별로 처리
     if #available(iOS 16.0, *) {
       url = baseURL.appending(path: directory, directoryHint: .isDirectory)
@@ -64,73 +64,76 @@ public final class DefaultLocalStorage: DataStorageInterface {
       url = baseURL.appendingPathComponent(directory, isDirectory: true)
       try createDirectoryIfNeeded(at: url)
     }
-
+    
     // MARK: - Create File
     do {
       let encodingData = try encoder.encode(data)
-
       let fileURL = url.appendingPathComponent(fileName)
       fileManager.createFile(atPath: fileURL.path, contents: encodingData)
     } catch {
-      throw IOError.writeError
+      throw StorageError.writeError
     }
   }
-
+  
   public func deleteDiary(timeStamp: String) throws {
     let (directory, fileName) = try parseTimeStamp(timeStamp)
     let url: URL
-
+    
     if #available(iOS 16.0, *) {
       url = baseURL.appending(path: directory, directoryHint: .isDirectory)
     } else {
       url = baseURL.appendingPathComponent(directory, isDirectory: true)
     }
-
+    
     let fileURL = url.appendingPathComponent(fileName)
-
+    
     // 파일이 이미 없는 경우 성공
     guard fileManager.fileExists(atPath: fileURL.path) else {
       return
     }
-
-    // 파일 삭제
-    try fileManager.removeItem(at: fileURL)
-
-    // 디렉토리가 비어있다면 디렉토리도 삭제
-    let directoryContents = try fileManager.contentsOfDirectory(
-      at: url,
-      includingPropertiesForKeys: nil
-    )
-
-    if directoryContents.isEmpty {
-      try fileManager.removeItem(at: url)
+    
+    do {
+      // 파일 삭제
+      try fileManager.removeItem(at: fileURL)
+    
+      // 디렉토리가 비어있다면 디렉토리도 삭제
+      let directoryContents = try fileManager.contentsOfDirectory(
+        at: url,
+        includingPropertiesForKeys: nil
+      )
+    
+      if directoryContents.isEmpty {
+        try fileManager.removeItem(at: url)
+      }
+    } catch {
+      throw StorageError.deleteError
     }
   }
-
+  
   //TODO: - 캐시삭제 구현 예정
 }
 
 private extension DefaultLocalStorage {
   func createDirectoryIfNeeded(at url: URL) throws {
     var isDirectory: ObjCBool = false
-
+    
     do {
       if !fileManager.fileExists(atPath: url.path, isDirectory: &isDirectory) {
         try fileManager.createDirectory(at: url, withIntermediateDirectories: true)
       }
     } catch {
-      throw IOError.writeError
+      throw StorageError.writeError
     }
   }
-
+  
   func parseTimeStamp(_ timeStamp: String) throws -> (String, String) {
     guard timeStamp.count == 14 else {
-      throw IOError.readError
+      throw StorageError.invalidInput
     }
-
+    
     let directory = String(timeStamp.prefix(8))
     let fileName = String(timeStamp.suffix(6))
-
+    
     return (directory, fileName)
   }
 }
