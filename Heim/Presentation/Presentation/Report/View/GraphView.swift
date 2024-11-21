66a890def6e933858da78ac6777d55a00047349c
futuremirae <kml8486@naver.com>
2024-11-21 21:07:57 +0900

    [Design] GraphView 파일 생성 완료

---
+++ Heim/Presentation/Presentation/Report/View/GraphView.swift
@@ -0,0 +1,15 @@
+//
+//  GraphView.swift
+//  Presentation
+//
+//  Created by 김미래 on 11/21/24.
+//
+
+import UIKit
+import Charts
+
+final class GraphView: UIView {
+  let chartYvalue = [1, 10, 3, 7, 8, 9,10]
+  private let chartEmotion: [UIImage] = []
+
+}
