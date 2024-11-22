997b1a577c55a64ce58745b32f00494b8a0a61f6
futuremirae <kml8486@naver.com>
2024-11-22 22:00:06 +0900

    [Feat] - #5 Chart생성
    - 막대그래프의 높이와 감정에 따른 생상을 결정합니다.

---
+++ Heim/Presentation/Presentation/Report/Model/Chart.swift
@@ -0,0 +1,18 @@
+//
+//  Chart.swift
+//  Presentation
+//
+//  Created by 김미래 on 11/22/24.
+//
+
+import UIKit
+
+struct Chart {
+  var value: Double
+  var color: UIColor
+
+  init(value: Double, color: UIColor) {
+    self.value = value
+    self.color = color
+  }
+}
