10440902c9643e3894f49471dc5fcb77129ef8c9
Hansangjin98 <onetable98@naver.com>
2024-11-23 20:54:25 +0900

    [Feat] #62 - HomeCoordinator 구현

---
+++ Heim/Presentation/Presentation/Home/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,71 @@
+//
+//  HomeCoordinator.swift
+//  Presentation
+//
+//  Created by 한상진 on 11/21/24.
+//
+
+import Core
+import Domain
+import UIKit
+
+public protocol HomeCoordinator: Coordinator {
+  func provideHomeViewController() -> HomeViewController
+  func pushDiaryDetailView(diary: Diary)
+}
+
+public final class DefaultHomeCoordinator: HomeCoordinator {
+  // MARK: - Properties
+  public weak var parentCoordinator: Coordinator?
+  public var childCoordinators: [Coordinator] = []
+  public var navigationController: UINavigationController
+
+  // MARK: - Initialize
+  public init(navigationController: UINavigationController) {
+    self.navigationController = navigationController
+  }
+  
+  // MARK: - Methods
+  public func start() {
+    guard let homeViewController = createHomeViewController() else { return }
+    navigationController.pushViewController(homeViewController, animated: true)
+  }
+  
+  public func provideHomeViewController() -> HomeViewController {
+    guard let homeViewController = createHomeViewController() else { return HomeViewController() }
+    return homeViewController
+  }
+  
+  public func didFinish() {
+    parentCoordinator?.removeChild(self)
+  }
+  
+  public func pushDiaryDetailView(diary: Diary) {
+    guard let defaultDiaryDetailCoordinator = DIContainer.shared.resolve(type: DiaryDetailCoordinator.self) else {
+      return
+    }
+    
+    addChild(defaultDiaryDetailCoordinator)
+    defaultDiaryDetailCoordinator.parentCoordinator = self
+    defaultDiaryDetailCoordinator.start(diary: diary)
+  }
+  
+  public func pushSettingView() {
+    guard let defaultSettingCoordinator = DIContainer.shared.resolve(type: SettingCoordinator.self) else {
+      return
+    }
+    
+    addChild(defaultSettingCoordinator)
+    defaultSettingCoordinator.parentCoordinator = self
+    defaultSettingCoordinator.start()
+  }
+}
+
+// MARK: - Private
+private extension DefaultHomeCoordinator {
+  func createHomeViewController() -> HomeViewController? {
+    let viewController = HomeViewController()
+    viewController.coordinator = self
+    return viewController
+  }
+}
