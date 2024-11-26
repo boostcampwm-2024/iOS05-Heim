e0550015284949f0d869874ee4bbebf3b15f0c99
ParkSeongGeun <phd0801@naver.com>
2024-11-26 15:55:43 +0900

    [Feat] #80 presentAlert(removeCache, resetData)

--- Heim/Presentation/Presentation/Interface/Alertable.swift
+++ Heim/Presentation/Presentation/Interface/Alertable.swift
@@ -11,28 +11,38 @@ protocol Alertable {}
 
 enum AlertType {
   case removeDiary
+  case removeCache // 캐시 삭제
+  case removeData  // 데이터 삭제
   
   var title: String {
     switch self {
     case .removeDiary: "나의 일기가 사라져요"
+    case .removeCache: "현재 기기에 저장된 일기가 모두 사라져요"
+    case .removeData: "현재 기기에 저장된 일기가 모두 사라져요"
     }
   }
   
   var message: String {
     switch self {
     case .removeDiary: "이 글은 더 이상 볼 수 없을텐데,\n정말 삭제하시겠어요?"
+    case .removeCache: "현재 기기에 저장된 일기가 사라져요,\n정말 삭제하시겠어요?"
+    case .removeData: "현재 기기에 저장된 일기가 사라져요,\n정말 삭제하시겠어요?"
     }
   }
   
   var leftButtonTitle: String {
     switch self {
     case .removeDiary: "다음에"
+    case .removeCache: "닫기"
+    case .removeData: "닫기"
     }
   }
   
   var rightButtonTitle: String {
     switch self {
     case .removeDiary: "삭제"
+    case .removeCache: "확인"
+    case .removeData: "확인"
     }
   }
 }
