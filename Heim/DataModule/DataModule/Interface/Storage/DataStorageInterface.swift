cf42de7a1fa528c285c48554a1bdc3665296aced
Hansangjin98 <onetable98@naver.com>
2024-11-28 03:23:19 +0900

    [Feat] #87 - DataStorage Interface 수정

--- Heim/DataModule/DataModule/Interface/Storage/DataStorageInterface.swift
+++ Heim/DataModule/DataModule/Interface/Storage/DataStorageInterface.swift
@@ -1,15 +1,16 @@
 //
-//  DataStorageModule.swift
+//  DataStorage.swift
 //  DataModule
 //
 //  Created by 박성근 on 11/20/24.
 //
 
+import Domain
 import Foundation
 
-public protocol DataStorageModule {
-  func readData<T: Decodable>(timeStamp: String) async throws -> T
-  func saveData<T: Encodable>(timeStamp: String, data: T) async throws
-  func deleteData(timeStamp: String) async throws
+public protocol DataStorage {
+  func readData<T: Decodable>(calendarDate: CalendarDate) async throws -> T
+  func saveData<T: Encodable>(calendarDate: CalendarDate, data: T) async throws
+  func deleteData(calendarDate: CalendarDate) async throws
   func deleteAll() async throws
 }
