cd626c80fa9956be7e936de4a5d31405e168feea
Hansangjin98 <onetable98@naver.com>
2024-11-28 03:17:48 +0900

    [Feat] #87 - CalendarCellModel 구현

---
+++ Heim/Presentation/Presentation/Home/Model/CalendarCellModel.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarCellModel.swift
+//  Presentation
+//
+//  Created by 한상진 on 11/27/24.
+//
+
+import Domain
+
+struct CalendarCellModel {
+  let day: String
+  let emotion: Emotion
+}
