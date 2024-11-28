da9e6c0f11792ec41e63ffe09727c5264e813b84
Jiyong <hickerd@sch.ac.kr>
2024-11-28 17:46:05 +0900

    [Feat] #107 - Connect ReportViewController

--- Heim/Presentation/Presentation/Report/ViewModel/ReportViewModel.swift
+++ Heim/Presentation/Presentation/Report/ViewModel/ReportViewModel.swift
@@ -9,10 +9,10 @@ import Combine
 import Core
 import Domain
 
-final class ReportViewModel: ViewModel {
+public final class ReportViewModel: ViewModel {
 
   // MARK: - Properties
-  enum Action {
+  public enum Action {
     //TODO: 수정
     case fetchData
   }
@@ -27,7 +27,7 @@ final class ReportViewModel: ViewModel {
     var angryCount: Int
   }
 
-  struct State: Equatable {
+  public struct State: Equatable {
     var userName: String
     var totalCount: Int
     var sequenceCount: Int
@@ -38,27 +38,31 @@ final class ReportViewModel: ViewModel {
   }
 
 // TODO: UseCase 추가
-  @Published var state: State
+  @Published public var state: State
 
   // MARK: - Initializer
   // TODO: Initializer에 UseCase 추가
   init() {
-    self.state = State(userName: "미래",
-                       totalCount: 0,
-                       sequenceCount: 0,
-                       monthCount: 0,
-                       emotionCount: EmotionCount(sadnessCount: 0,
-                                                  happinessCount: 0,
-                                                  surpriseCount: 0,
-                                                  fearCount: 0,
-                                                  disgustCount: 0,
-                                                  neutralCount: 0,
-                                                  angryCount: 0),
-                       emotion: "",
-                       reply: "답장이 도착하지 않았어요!")
+    self.state = State(
+      userName: "미래",
+      totalCount: 0,
+      sequenceCount: 0,
+      monthCount: 0,
+      emotionCount: EmotionCount(
+        sadnessCount: 0,
+        happinessCount: 0,
+        surpriseCount: 0,
+        fearCount: 0,
+        disgustCount: 0,
+        neutralCount: 0,
+        angryCount: 0
+      ),
+      emotion: "",
+      reply: "답장이 도착하지 않았어요!"
+    )
   }
 
-  func action(_ action: Action) {
+  public func action(_ action: Action) {
     switch action {
     case .fetchData:
       fetchData()
