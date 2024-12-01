036a5c0ec317bac0f36a95d6f2c47fcd3bd2c636
Hansangjin98 <onetable98@naver.com>
2024-12-02 03:11:31 +0900

    [Feat] #100 - DiaryUseCase 기능 확장
    - 전체 일기 데이터를 불러오는 함수 구현
    - 일기 연속 작성일을 계산하는 함수 구현
    - 최근 한 달간 작성한 일기 수를 계산하는 함수 구현

--- Heim/Domain/Domain/UseCase/DiaryUseCase.swift
+++ Heim/Domain/Domain/UseCase/DiaryUseCase.swift
@@ -13,11 +13,11 @@ public protocol DiaryUseCase {
 
   // MARK: - Methods
   func readDiaries(calendarDate: CalendarDate) async throws -> [Diary]
+  func readTotalDiaries() async throws -> [Diary]
   func saveDiary(data: Diary) async throws
-  // TODO: 인덱스 접근 고려
-
   func deleteDiary(calendarDate: CalendarDate) async throws
-
+  func fetchContinuousCount() async throws -> Int
+  func fetchMonthCount() async throws -> Int
 }
 
 public struct DefaultDiaryUseCase: DiaryUseCase {
@@ -34,6 +34,10 @@ public struct DefaultDiaryUseCase: DiaryUseCase {
     return try await diaryRepository.readDiaries(calendarDate: calendarDate)
   }
   
+  public func readTotalDiaries() async throws -> [Diary] {
+    return try await diaryRepository.readTotalDiaries()
+  }
+  
   public func saveDiary(data: Diary) async throws {
     try await diaryRepository.saveDiary(data: data)
   }
@@ -41,8 +45,57 @@ public struct DefaultDiaryUseCase: DiaryUseCase {
   public func deleteDiary(calendarDate: CalendarDate) async throws {
     try await diaryRepository.deleteDiary(calendarDate: calendarDate)
   }
-
-  public func countTotalDiary() async throws -> Int {
-    return try await diaryRepository.countTotalDiary()
+  
+  public func fetchContinuousCount() async throws -> Int {
+    var count = 0
+    let calendar = Calendar.current
+    var currentDate = Date()
+    var endDate = calendar.range(of: .day, in: .month, for: currentDate)?.count ?? Int()
+    var currentDiaries: [Diary] = try await diaryRepository.readDiaries(calendarDate: currentDate.calendarDate())
+    var currentDay = currentDate.calendarDate().day
+    
+    func countDiary(_ diaries: [Diary]) async throws {
+      for diary in diaries.sorted(by: { $0.calendarDate.day > $1.calendarDate.day }) {
+        if diary.calendarDate.day == currentDay {
+          count += 1
+        }
+        
+        if currentDay - 1 > 0 {
+          currentDay -= 1
+        } else {
+          currentDate = calendar.date(byAdding: DateComponents(month: -1), to: currentDate) ?? Date()
+          endDate = calendar.range(of: .day, in: .month, for: currentDate)?.count ?? Int()
+          currentDay = endDate
+          currentDiaries = try await diaryRepository.readDiaries(calendarDate: currentDate.calendarDate())
+          try await countDiary(currentDiaries)
+        }
+      }
+    }
+    
+    try await countDiary(currentDiaries)
+    return count
+  }
+  
+  public func fetchMonthCount() async throws -> Int {
+    let date = Date()
+    var calendarDate = date.calendarDate()
+    let targetDay = calendarDate.day
+    var diaries: [Diary] = try await diaryRepository.readDiaries(calendarDate: calendarDate)
+    var count = diaries.count
+    
+    if calendarDate.day < 30 {
+      guard let lastDate = Calendar.current.date(byAdding: DateComponents(month: -1), to: date) else {
+        return count
+      }
+      
+      calendarDate = lastDate.calendarDate()
+      diaries = try await diaryRepository.readDiaries(calendarDate: calendarDate)
+      
+      for diary in diaries.reversed() where calendarDate.day >= targetDay {
+        count += 1
+      }
+    }
+    
+    return count
   }
 }
