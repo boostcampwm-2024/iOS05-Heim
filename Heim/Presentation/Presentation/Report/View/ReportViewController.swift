d2d1703afb98f82b25a0df91a94bf0d22ea6c2b6
Hansangjin98 <onetable98@naver.com>
2024-12-02 03:16:57 +0900

    [Feat] #100 - ReportViewController 기능 구현

--- Heim/Presentation/Presentation/Report/View/ReportViewController.swift
+++ Heim/Presentation/Presentation/Report/View/ReportViewController.swift
@@ -7,7 +7,7 @@
 import Domain
 import UIKit
 
-final class ReportViewController: BaseViewController<ReportViewModel>, Coordinatable {
+public final class ReportViewController: BaseViewController<ReportViewModel>, Coordinatable {
   // MARK: - Properties
   weak var coordinator: DefaultReportCoordinator?
 
@@ -17,88 +17,196 @@ final class ReportViewController: BaseViewController<ReportViewModel>, Coordinat
     scrollView.showsVerticalScrollIndicator = false
     return scrollView
   }()
-
-  private let reportView: ReportView = {
-    let reportView = ReportView()
-    reportView.backgroundColor = .clear
-    return reportView
+  
+  private let contentView = UIView()
+  private let titleLabel = CommonLabel(text: "하임이와 함께 한 기록", font: .bold, size: LayoutConstants.titleOne)
+  
+  private let totalReportView: ReportCountView = {
+    let reportCountView = ReportCountView()
+    reportCountView.layer.cornerRadius = 8
+    reportCountView.layer.borderWidth = 1
+    reportCountView.layer.borderColor = UIColor.white.cgColor
+    return reportCountView
   }()
+  
+  private let emotionLabel = CommonLabel(textAlignment: .center, font: .regular, size: LayoutConstants.titleThree)
+  private let graphView = GraphView()
+  
+  private let replyTitleLabel = CommonLabel(
+    text: "하임이의 답장",
+    font: .bold,
+    size: LayoutConstants.titleTwo,
+    textColor: .white
+  )
+  
+  private let replyTextView = CommonTextAreaView()
 
   // MARK: - LifeCycle
-  override func viewDidLoad() {
+  public override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     setupViews()
     setupLayoutConstraints()
-    viewModel.action(.fetchData)
+  }
+  
+  public override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    viewModel.action(.fetchTotalDiaryCount)
+    viewModel.action(.fetchContinuousCount)
+    viewModel.action(.fetchMonthCount)
   }
 
-  override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-
-    // TODO: 수정
-    //coordinator?.didFinish()
+  deinit {
+    coordinator?.didFinish()
   }
 
   // MARK: - LayOut Methods
   override func setupViews() {
     super.setupViews()
-
+    
     view.addSubview(scrollView)
-    scrollView.addSubview(reportView)
+    scrollView.addSubview(contentView)
+    contentView.addSubview(titleLabel)
+    contentView.addSubview(totalReportView)
+    contentView.addSubview(emotionLabel)
+    contentView.addSubview(graphView)
+    contentView.addSubview(replyTitleLabel)
+    contentView.addSubview(replyTextView)
   }
 
   override func setupLayoutConstraints() {
     super.setupLayoutConstraints()
+    
     scrollView.snp.makeConstraints {
-      $0.edges.equalToSuperview()
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.bottom.equalToSuperview()
     }
 
-    reportView.snp.makeConstraints {
+    contentView.snp.makeConstraints {
       $0.edges.width.equalToSuperview()
-      $0.height.equalToSuperview()
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.top.equalToSuperview().offset(LayoutConstants.defaultPadding)
+      $0.centerX.equalToSuperview()
+    }
+    
+    totalReportView.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(LayoutConstants.defaultPadding)
+      $0.centerX.equalToSuperview()
+    }
+
+    emotionLabel.snp.makeConstraints {
+      $0.top.equalTo(totalReportView.snp.bottom).offset(LayoutConstants.defaultPadding * 3)
+      $0.centerX.equalToSuperview()
+    }
+
+    graphView.snp.makeConstraints {
+      $0.top.equalTo(emotionLabel.snp.bottom).offset(LayoutConstants.graphViewTop)
+      $0.height.equalTo(UIApplication.screenHeight * LayoutConstants.graphViewHeight)
+      $0.leading.trailing.equalToSuperview().inset(LayoutConstants.graphViewInset)
+    }
+
+    replyTitleLabel.snp.makeConstraints {
+      $0.top.equalTo(graphView.snp.bottom).offset(LayoutConstants.replyTitleLabelTop)
+      $0.centerX.equalToSuperview()
+    }
+
+    replyTextView.snp.makeConstraints {
+      $0.top.equalTo(replyTitleLabel.snp.bottom).offset(LayoutConstants.defaultPadding)
+      $0.leading.trailing.equalToSuperview().inset(LayoutConstants.defaultPadding)
+      $0.bottom.lessThanOrEqualToSuperview().offset(-LayoutConstants.bottomPadding)
     }
   }
-
-  override func bindState() {
-    super.bindState()
-
-    viewModel.$state
-      .map(\.userName)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] userName in
-        self?.reportView.updateUserNameLabel(name: userName)
-      }
-      .store(in: &cancellable)
-
-    viewModel.$state
-      .map(\.reply)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] reply in
-        self?.reportView.replyTextView.setText(reply)
-      }
-      .store(in: &cancellable)
-
-    viewModel.$state
-      .map(\.emotion)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] emotion in
-        self?.reportView.updateEmotionLabel(emotion: emotion)
-      }
-      .store(in: &cancellable)
-
-    viewModel.$state
-      .map(\.emotionValue)
-      .receive(on: DispatchQueue.main)
-      .sink { [weak self] emotionValue in
-//        self?.reportView.updateEmotionLabel(emotion: emotionCount)
-      }
-      .store(in: &cancellable)
-
-  }
-
 
   override func bindAction() {
+    super.bindAction()
+    
+    viewModel.action(.fetchUserName)
+  }
+  
+  override func bindState() {
+    viewModel.$state
+      .map { (userName: $0.userName, emotionTitle: $0.mainEmotionTitle) }
+      .removeDuplicates { (lhs, rhs) in
+        let duplicated = (lhs.userName == rhs.userName) && (lhs.emotionTitle == rhs.emotionTitle)
+        return duplicated
+      }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] (userName, emotionTitle) in
+        self?.updateEmotionLabel(userName: userName, emotionTitle: emotionTitle)
+      }
+      .store(in: &cancellable)
+    
+    viewModel.$state
+      .map { (total: $0.totalCount, continuous: $0.continuousCount, month: $0.monthCount) }
+      .removeDuplicates { (lhs, rhs) in
+        let duplicated = (lhs.total == rhs.total) && (lhs.continuous == rhs.continuous) && (lhs.month == rhs.month)
+        return duplicated
+      }
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] (total, continuous, month) in
+        self?.totalReportView.updateCountLabels(total: total, continuous: continuous, month: month)
+      }
+      .store(in: &cancellable)
+    
+    viewModel.$state
+      .map { $0.reply }
+      .removeDuplicates()
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] in
+        self?.replyTextView.setText($0)
+      }
+      .store(in: &cancellable)
+    
+    viewModel.$state
+      .map { $0.emotionCountDictionary }
+      .removeDuplicates()
+      .receive(on: DispatchQueue.main)
+      .sink { [weak self] in
+        self?.graphView.configureChart($0)
+        self?.updateReplyAreaLayout(by: $0)
+      }
+      .store(in: &cancellable)
   }
 }
 
+// MARK: - Private Extenion
+private extension ReportViewController {
+  func updateEmotionLabel(userName: String, emotionTitle: String) {
+    guard !emotionTitle.isEmpty else {
+      emotionLabel.text = "아직 \(userName)님께서 작성하신\n 일기가 없습니다!"
+      return
+    }
+    
+    let fullText = "지난 30일간 \(userName)께서 \n가장 많이 느끼신 감정은 \(emotionTitle)이군요"
+    let attributedString = NSMutableAttributedString(string: fullText)
+    let emotionFont = UIFont.boldFont(ofSize: LayoutConstants.titleThree)
+    let range = (fullText as NSString).range(of: emotionTitle)
+    attributedString.addAttribute(.font, value: emotionFont, range: range)
+    emotionLabel.attributedText = attributedString
+  }
+  
+  func updateReplyAreaLayout(by emotionCountDictionary: [Emotion: Int]) {
+    let emotionIsEmpty = emotionCountDictionary.values.reduce(0, +) < 1
+    
+    replyTitleLabel.snp.remakeConstraints {
+      let target = emotionIsEmpty ? emotionLabel.snp.bottom : graphView.snp.bottom
+      $0.top.equalTo(target).offset(LayoutConstants.replyTitleLabelTop)
+      $0.centerX.equalToSuperview()
+    }
+  }
+  
+  enum LayoutConstants {
+    static let defaultPadding: CGFloat = 16
+    static let titleOne: CGFloat = 28
+    static let titleTwo: CGFloat = 24
+    static let titleThree: CGFloat = 20
+    static let graphViewHeight: CGFloat = 0.257
+    static let graphViewInset: CGFloat = 66
+    static let graphViewTop: CGFloat = 32
+    static let replyTitleLabelTop: CGFloat = 32
+    static let bottomPadding: CGFloat = UIApplication.screenHeight * 0.148
+  }
+}
