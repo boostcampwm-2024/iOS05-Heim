d44a0e7076873a88c9c0aa1d4463c2e1aff07975
Jiyong <hickerd@sch.ac.kr>
2024-12-01 18:55:02 +0900

    [Fix] #121 - Fix a bug where the waveform was too big

--- Heim/Presentation/Presentation/DiaryReplay/View/VisualizerView.swift
+++ Heim/Presentation/Presentation/DiaryReplay/View/VisualizerView.swift
@@ -5,48 +5,66 @@
 //  Created by 정지용 on 11/28/24.
 //
 
+import AVFoundation
 import UIKit
 
 final class VisualizerView: UIView {
   private var displayLink: CADisplayLink?
   private weak var viewModel: DiaryReplayViewModel?
-  private var defaultAmplitude: CGFloat = 0.01
-
+  private var defaultAmplitude: CGFloat = 1
+  private var maxDecibel: Float = -160.0
+  private let totalBars = 30
+  private let amplitudeScalingFactor: CGFloat = 1.5
+  
   func startVisualizer(for viewModel: DiaryReplayViewModel?) {
     self.viewModel = viewModel
     displayLink?.invalidate()
     displayLink = CADisplayLink(target: self, selector: #selector(updateVisualizer))
     displayLink?.add(to: .main, forMode: .common)
+    
+    guard let replayManager = viewModel?.diaryReplayManager else { return }
+    replayManager.calculateMaxDecibel { [weak self] maxDecibel in
+      DispatchQueue.main.async {
+        self?.maxDecibel = max(maxDecibel ?? -160.0, -60)
+      }
+    }
   }
-
+  
   func stopVisualizer() {
     displayLink?.invalidate()
     displayLink = nil
   }
-
+  
   @objc
   private func updateVisualizer() {
     guard let manager = viewModel?.diaryReplayManager else { return }
     
     manager.audioPlayer.updateMeters()
+    
     let power = manager.audioPlayer.averagePower(forChannel: 0)
-    let amplitude = pow(10, power / 20)
-    updateVisualizerPath(amplitude: CGFloat(max(CGFloat(amplitude) * 15, defaultAmplitude)))
+    let amplitude = calculateAmplitude(for: power)
+    updateVisualizerPath(amplitude: amplitude)
   }
-
+  
+  private func calculateAmplitude(for power: Float) -> CGFloat {
+    let maxAmplitude = CGFloat(pow(10, maxDecibel / 20))
+    let currentAmplitude = CGFloat(pow(10, power / 20))
+    let normalizedAmplitude = currentAmplitude / maxAmplitude
+    return max(normalizedAmplitude * bounds.height, defaultAmplitude)
+  }
+  
   private func updateVisualizerPath(amplitude: CGFloat) {
     layer.sublayers?.forEach { $0.removeFromSuperlayer() }
     
     let path = UIBezierPath()
+    let barWidth = bounds.width / CGFloat(totalBars)
+    let maxHeight = bounds.height
     let centerY = bounds.midY
-    let totalBars = 20
-    let barWidth: CGFloat = bounds.width / CGFloat(totalBars)
-    let maxHeight = bounds.height / 5
     
-    for i in 0..<totalBars {
-      let normalizedAmplitude = amplitude * CGFloat.random(in: 0.5...1.5)
-      let barHeight = normalizedAmplitude * maxHeight
-      let xPosition = CGFloat(i) * barWidth
+    for sequence in 0..<totalBars {
+      let randomizedAmplitude = amplitude * CGFloat.random(in: 0.5...amplitudeScalingFactor)
+      let barHeight = min(randomizedAmplitude, maxHeight)
+      let xPosition = CGFloat(sequence) * barWidth
       let barRect = CGRect(
         x: xPosition,
         y: centerY - barHeight / 2,
