a74070c2f000fc9085403b413e1f3289db7594d8
Hansangjin98 <onetable98@naver.com>
2024-12-02 03:15:45 +0900

    [Feat] #100 - GraphView 기능 구현

--- Heim/Presentation/Presentation/Report/View/GraphView.swift
+++ Heim/Presentation/Presentation/Report/View/GraphView.swift
@@ -5,95 +5,104 @@
 //  Created by 김미래 on 11/22/24.
 //
 
+import Domain
 import UIKit
 
 final class GraphView: UIView {
-  // TODO: mock데이터 제거 및 color 추가
-  let emotionCharts: [Chart] =
-  [Chart(value: 0.1, emotion: .sadness),
-   Chart(value: 1.0, emotion: .angry),
-   Chart(value: 0.3, emotion: .disgust),
-   Chart(value: 0.7, emotion: .fear),
-   Chart(value: 0.6, emotion: .happiness),
-   Chart(value: 0.5, emotion: .neutral),
-   Chart(value: 0.5, emotion: .surprise)]
-
-  private let emotionEmojis = HeimEmoji.defaultEmotions()
-
+  // MARK: - Properties
   private let graphStackView: UIStackView = {
     let stackView = UIStackView()
-    stackView.spacing = LayoutConstants.defaultPadding
+    stackView.spacing = LayoutConstants.stackViewPadding
     stackView.distribution = .fillEqually
     return stackView
   }()
 
   private let emotionStack: UIStackView = {
     let stackView = UIStackView()
+    stackView.spacing = LayoutConstants.stackViewPadding
     stackView.distribution = .fillEqually
     return stackView
   }()
+  
+  private let underLineView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .white.withAlphaComponent(0.5)
+    return view
+  }()
 
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
+  // MARK: - Initializer
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupViews()
     setupLayoutConstraints()
-    setupGraphStackView()
-    setupEmojiStackView()
   }
-  // TODO: [Chart]파라미터로 하는 Initializer 만들기
-
-  func updateGraphView() {
-    emotionCharts.forEach {
-      let barView = BarView(chart: $0)
-      graphStackView.addArrangedSubview(barView)
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Methods
+  func configureChart(_ emotionCountDictionary: [Emotion: Int]) {
+    graphStackView.arrangedSubviews.forEach {
+      $0.removeFromSuperview()
     }
-
+    
+    emotionStack.arrangedSubviews.forEach {
+      $0.removeFromSuperview()
+    }
+    
+    var charts: [Chart] = []
+    let totalCount = emotionCountDictionary.values.reduce(0, +)
+    guard totalCount > 0 else { return }
+    
+    emotionCountDictionary.forEach {
+      charts.append(Chart(
+        value: CGFloat($0.value) / CGFloat(totalCount), 
+        emotion: $0.key
+      ))
+    }
+    charts.sort { $0.value > $1.value }
+    
+    charts.forEach {
+      graphStackView.addArrangedSubview(BarView(chart: $0))
+      emotionStack.addArrangedSubview(HeimEmojiView(emotion: $0.emotion))
+    }
   }
-
 }
 
 // MARK: - Layout
 private extension GraphView {
-  enum LayoutConstants {
-    static let defaultPadding: CGFloat = 13
-    static let graphViewHeightRatio: CGFloat = 0.7
-    static let emotionViewHeightRatio: CGFloat = 0.3
-  }
-
   func setupViews() {
     addSubview(graphStackView)
+    addSubview(underLineView)
     addSubview(emotionStack)
-  }
-
-  func setupGraphStackView() {
-    emotionCharts.forEach {
-      let barView = BarView(chart: $0)
-      graphStackView.addArrangedSubview(barView)
-    }
-  }
-
-  func setupEmojiStackView() {
-    emotionEmojis.forEach { emotionEmoji in
-      emotionEmoji.contentMode = .scaleAspectFill
-      emotionStack.addArrangedSubview(emotionEmoji)
-    }
   }
 
   func setupLayoutConstraints() {
     graphStackView.snp.makeConstraints {
       $0.top.equalToSuperview()
       $0.leading.trailing.equalToSuperview()
-      $0.height.equalTo(self.snp.height).multipliedBy(LayoutConstants.graphViewHeightRatio)
+      $0.height.equalToSuperview().multipliedBy(LayoutConstants.graphViewHeightRatio)
+    }
+    
+    underLineView.snp.makeConstraints {
+      $0.top.equalTo(graphStackView.snp.bottom)
+      $0.height.equalTo(LayoutConstants.underLineView)
+      $0.leading.trailing.equalTo(graphStackView)
     }
 
     emotionStack.snp.makeConstraints {
-      $0.top.equalTo(graphStackView.snp.bottom).offset(LayoutConstants.defaultPadding)
+      $0.top.equalTo(graphStackView.snp.bottom).offset(LayoutConstants.emotionPadding)
       $0.leading.trailing.equalTo(graphStackView)
-      $0.height.equalToSuperview().multipliedBy(LayoutConstants.emotionViewHeightRatio)
+      $0.height.equalTo(emotionStack.snp.width).multipliedBy(LayoutConstants.emotionViewHeightRatio)
     }
+  }
+  
+  enum LayoutConstants {
+    static let stackViewPadding: CGFloat = 8
+    static let emotionPadding: CGFloat = 5
+    static let underLineView: CGFloat = 2
+    static let graphViewHeightRatio: CGFloat = 0.838
+    static let emotionViewHeightRatio: CGFloat = 0.113
   }
 }
