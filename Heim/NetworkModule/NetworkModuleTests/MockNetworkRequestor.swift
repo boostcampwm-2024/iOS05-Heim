ae36bebaa425eceebfcceb145407ed34cd88ef4e
futuremirae <kml8486@naver.com>
2024-12-05 18:21:09 +0900

    [Test] - #176 MockRequestor생성

---
+++ Heim/NetworkModule/NetworkModuleTests/MockNetworkRequestor.swift
@@ -0,0 +1,45 @@
+//
+//  MockNetworkRequestor.swift
+//  NetworkModule
+//
+//  Created by 김미래 on 12/5/24.
+//
+
+import Foundation
+import NetworkModule
+import Domain
+
+final class MockNetworkRequestor: NetworkRequestable {
+  // MARK: - Properties
+  var response: URLResponse?
+  var data: Data?
+  var error: Error?
+
+  // MARK: - Methods
+  func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+    if let error = error {
+      throw error
+    }
+    guard let data = data, let response = response else {
+      throw NetworkError.interalServerError
+    }
+
+    return (data, response)
+  }
+
+  func setURLResponse(_ urlResponse: URLResponse?) {
+    error = nil
+    self.response = urlResponse
+  }
+
+  func setResponseData(_ responseData: Data?) {
+    error = nil
+    self.data = responseData
+  }
+
+  func setError(_ errorReponse: Error?) {
+    response = nil
+    data = nil
+    self.error = errorReponse
+  }
+}
